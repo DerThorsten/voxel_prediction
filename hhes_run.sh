393a9eef737fab8fea9b0669492d33b0ff9a6bc3
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2015-07-04 11:08:41 +0200

    rb

--- hhes_run.sh
+++ hhes_run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-python voxel_prediction.py example_files/hhes/hhes.json  \
-    -m train 
+#python voxel_prediction.py example_files/hhes/hhes.json  \
+#    -m train 
 
 # tiny roi
-python voxel_prediction.py example_files/hhes/hhes.json  \
-    -m test -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_3.h5 -k data --out hhes_3_n_out.h5 -rb 100 100 100 -re 300 300 200
+#python voxel_prediction.py example_files/hhes/hhes.json  \
+#    -m test -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_3.h5 -k data --out hhes_3_n_out.h5 -rb 100 100 100 -re 300 300 200
 
 # 
-python voxel_prediction.py example_files/hhes/hhes.json  -m test \
-    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_1.h5 -k data \
-    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n.h5
-
-python voxel_prediction.py example_files/hhes/hhes.json  -m test \
-    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_2.h5 -k data \
-    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_2.h5
-    
-python voxel_prediction.py example_files/hhes/hhes.json  -m test \
-    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_3.h5 -k data \
-    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_3.h5
-
-python voxel_prediction.py example_files/hhes/hhes.json  -m test \
-    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_4.h5 -k data \
-    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_4.h5
-
-python voxel_prediction.py example_files/hhes/hhes.json  -m test \
-    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_5.h5 -k data \
-    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_5.h5  
-
-python voxel_prediction.py example_files/hhes/hhes.json  -m test \
-    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_6.h5 -k data \
-    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_6.h5  
+#python voxel_prediction.py example_files/hhes/hhes.json  -m test \
+#    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_1.h5 -k data \
+#    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n.h5
+#
+#python voxel_prediction.py example_files/hhes/hhes.json  -m test \
+#    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_2.h5 -k data \
+#    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_2.h5
+#    
+#python voxel_prediction.py example_files/hhes/hhes.json  -m test \
+#    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_3.h5 -k data \
+#    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_3.h5
+#
+#python voxel_prediction.py example_files/hhes/hhes.json  -m test \
+#    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_4.h5 -k data \
+#    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_4.h5
+#
+#python voxel_prediction.py example_files/hhes/hhes.json  -m test \
+#    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_5.h5 -k data \
+#    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_5.h5  
+#
+#python voxel_prediction.py example_files/hhes/hhes.json  -m test \
+#    -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_6.h5 -k data \
+#    --out /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/predictions/semantic/p_data_sub_n_6.h5  
 
 python voxel_prediction.py example_files/hhes/hhes.json  -m test \
     -d /media/tbeier/data/datasets/hhess/2x2x2nm_chunked/data_sub_n_7.h5 -k data \
